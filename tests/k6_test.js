8b5faa73bc6bcd2a9c3d0e07605357557d7dc400
Junrapong Borin <6431501013@lamduan.mfu.ac.th>
2025-10-16 02:33:49 +0700

    localhost

--- tests/k6_test.js
+++ tests/k6_test.js
@@ -14,7 +14,7 @@ export let options = {
   },
 };
 
-const BASE_URL = 'http://host.docker.internal:3000';
+const BASE_URL = "http://localhost:3000";
 
 export default function () {
   // เรียก API GET /books
